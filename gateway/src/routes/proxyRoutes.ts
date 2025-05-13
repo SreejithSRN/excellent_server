f6f2545f8edee5c26be61bb67109099c28421f36
SreejithSRN <sreejimavoor@gmail.com>
2025-05-13 17:05:23 +0530

    fixed the problem of header in gateway

--- gateway/src/routes/proxyRoutes.ts
+++ gateway/src/routes/proxyRoutes.ts
@@ -20,10 +20,15 @@ gatewayRoutes.forEach(({ path, role, service }) => {
     },
     proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
       proxyReqOpts.headers = proxyReqOpts.headers || {};
+      // if (srcReq.user) {
+      //   proxyReqOpts.headers['x-user-id'] = srcReq.user._id;
+      //   proxyReqOpts.headers['x-user-email'] = srcReq.user.email;
+      //   proxyReqOpts.headers['x-user-role'] = srcReq.user.role;
+      // }
       if (srcReq.user) {
-        proxyReqOpts.headers['x-user-id'] = srcReq.user._id;
-        proxyReqOpts.headers['x-user-email'] = srcReq.user.email;
-        proxyReqOpts.headers['x-user-role'] = srcReq.user.role;
+        (proxyReqOpts.headers as { [key: string]: string })['x-user-id'] = srcReq.user._id;
+        (proxyReqOpts.headers as { [key: string]: string })['x-user-email'] = srcReq.user.email;
+        (proxyReqOpts.headers as { [key: string]: string })['x-user-role'] = srcReq.user.role;
       }
       return proxyReqOpts;
     }
@@ -42,46 +47,5 @@ export default router;
 
 
 
-
-
-
-
-// import { Router } from "express";
-// import proxy from "express-http-proxy";
-// import { roleAuthMiddleware } from "../middleware/jwt";
-// import { gatewayRoutes } from "../middleware/gatewayConfig";
-
-// const router = Router();
-
-// // Public Auth Routes (like login/signup)
-
-// // const AUTH_SERVICE = process.env.AUTH_SERVICE!;
-// // router.use("/api/auth", proxy(AUTH_SERVICE));
-
-// const NOTIFICATION_SERVICE= process.env.NOTIFICATION_SERVICE!;
-// router.use("/api/notification", proxy(NOTIFICATION_SERVICE));
-
-// // Role-restricted service routes with path rewriting
-// gatewayRoutes.forEach(({ path, role, service }) => {
-//   router.use(path, roleAuthMiddleware(role), proxy(service, {
-//     proxyReqPathResolver: (req) => {
-//       // This only strips the base path (e.g., /api/course)
-//       const strippedPath = req.originalUrl.replace(/^\/api\/[^/]+/, "");
-//       return strippedPath; // DO NOT append query params — proxy will do it automatically
-//     },
-//     proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
-//       proxyReqOpts.headers = proxyReqOpts.headers || {};
-//       if (srcReq.user) {
-//         proxyReqOpts.headers['x-user-id'] = srcReq.user._id;
-//         proxyReqOpts.headers['x-user-email'] = srcReq.user.email;
-//         proxyReqOpts.headers['x-user-role'] = srcReq.user.role;
-//       }
-//       return proxyReqOpts;
-//     }
-//   }));
-//   ;
-// });
-
-// export default router;
 
 
